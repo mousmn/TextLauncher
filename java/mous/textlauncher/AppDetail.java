7a4308905cc4808b853c84ed89d04394428b5383
Dakota Holder <dakotaholder@knology.net>
2015-08-15 15:24:55 -0400

    beginnings

---
+++ java/mous/textlauncher/AppDetail.java
@@ -0,0 +1,12 @@
+package mous.textlauncher;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by dakota on 8/15/15.
+ */
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
